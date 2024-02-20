058c9aa1910e585105e7ab2e49709de3d438692c
SupakornThongaerd <supakorn642@gmail.com>
2024-02-20 14:32:32 +0700

    up

--- script.js
+++ script.js
@@ -27,17 +27,5 @@
 			}
 	});	
 	
-	//Switch light/dark
-	
-	$("#switch").on('click', function () {
-		if ($("body").hasClass("dark")) {
-			$("body").removeClass("dark");
-			$("#switch").removeClass("switched");
-		}
-		else {
-			$("body").addClass("dark");
-			$("#switch").addClass("switched");
-		}
-	});  
 	
   })(jQuery);
